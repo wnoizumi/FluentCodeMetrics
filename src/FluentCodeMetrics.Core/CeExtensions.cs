ab5a278b2d1777c8702d334028e8adefe989d08e
elemarjr <elemarjr@gmail.com>
2012-05-03 12:42:22 -0300

    Using Mono.Cecil to read IL code (finding Newobj instructions)

--- src/FluentCodeMetrics.Core/CeExtensions.cs
+++ src/FluentCodeMetrics.Core/CeExtensions.cs
@@ -2,6 +2,9 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
+using Mono.Cecil;
+using FluentCodeMetrics.Core.Cecil;
+using Mono.Cecil.Cil;
 
 namespace FluentCodeMetrics.Core
 {
@@ -14,68 +17,83 @@ namespace FluentCodeMetrics.Core
                 .Count();
         }
 
-public static IEnumerable<Type>
-    GetReferencedTypes(this Type that)
-{
-    var flags = BindingFlags.Instance |
-                BindingFlags.NonPublic |
-                BindingFlags.Public
-        ;
+        public static IEnumerable<Type>
+            GetReferencedTypes(this Type that)
+        {
 
-    var typeMetaAttributeTypes =
-        from attribute in that.GetCustomAttributes(true)
-        select attribute.GetType();
+            var assembly = AssemblyCache.Load(that.Assembly.GetName().Name);
+            var typeDef = assembly.MainModule.Types
+                .First(type => type.FullName == that.FullName);
 
-    var fieldMetaAttributeTypes =
-        from field in that.GetFields(flags)
-        from attribute in field.GetCustomAttributes(true)
-        select attribute.GetType();
+            var newObjTypes =
+                from method in typeDef.Methods
+                from instruction in method.Body.Instructions
+                where instruction.OpCode == OpCodes.Newobj
+                let operand = instruction.Operand as MethodReference
+                let typeFullName = operand.DeclaringType.FullName
+                let type = Type.GetType(typeFullName)
+                where type != null
+                select type;
+            
+            const BindingFlags flags = BindingFlags.Instance |
+                                       BindingFlags.NonPublic |
+                                       BindingFlags.Public;
 
-    var methodMetaAttributeTypes =
-        from method in that.GetMethods(flags)
-        from attribute in method.GetCustomAttributes(true)
-        select attribute.GetType();
+            var typeMetaAttributeTypes =
+                from attribute in that.GetCustomAttributes(true)
+                select attribute.GetType();
 
-    var methodParameterMetaAttributeTypes =
-        from method in that.GetMethods(flags)
-        from parameter in method.GetParameters()
-        from attribute in parameter.GetCustomAttributes(true)
-        select attribute.GetType();
-        
-    var fieldTypes =
-        from field in that.GetFields(flags)
-        select field.FieldType;
+            var fieldMetaAttributeTypes =
+                from field in that.GetFields(flags)
+                from attribute in field.GetCustomAttributes(true)
+                select attribute.GetType();
 
-    var propertyTypes =
-        from property in that.GetProperties(flags)
-        select property.PropertyType;
+            var methodMetaAttributeTypes =
+                from method in that.GetMethods(flags)
+                from attribute in method.GetCustomAttributes(true)
+                select attribute.GetType();
 
-    var methodReturnTypes =
-        from method in that.GetMethods(flags)
-        where method.ReturnType != typeof (void)
-        select method.ReturnType;
+            var methodParameterMetaAttributeTypes =
+                from method in that.GetMethods(flags)
+                from parameter in method.GetParameters()
+                from attribute in parameter.GetCustomAttributes(true)
+                select attribute.GetType();
 
-    var methodParameterTypes =
-        from method in that.GetMethods(flags)
-        from parameter in method.GetParameters()
-        select parameter.ParameterType;
+            var fieldTypes =
+                from field in that.GetFields(flags)
+                select field.FieldType;
 
-    var ctorParameterTypes =
-        from ctor in that.GetConstructors(flags)
-        from parameter in ctor.GetParameters()
-        select parameter.ParameterType;
-    
-    return new[] { that.BaseType }
-        .Union(typeMetaAttributeTypes)
-        .Union(fieldMetaAttributeTypes)
-        .Union(methodMetaAttributeTypes)
-        .Union(methodParameterMetaAttributeTypes)
-        .Union(ctorParameterTypes)
-        .Union(methodParameterTypes)
-        .Union(fieldTypes)
-        .Union(propertyTypes)
-        .Union(methodReturnTypes)
-        .Distinct();
-}
+            var propertyTypes =
+                from property in that.GetProperties(flags)
+                select property.PropertyType;
+
+            var methodReturnTypes =
+                from method in that.GetMethods(flags)
+                where method.ReturnType != typeof(void)
+                select method.ReturnType;
+
+            var methodParameterTypes =
+                from method in that.GetMethods(flags)
+                from parameter in method.GetParameters()
+                select parameter.ParameterType;
+
+            var ctorParameterTypes =
+                from ctor in that.GetConstructors(flags)
+                from parameter in ctor.GetParameters()
+                select parameter.ParameterType;
+
+            return new[] { that.BaseType }
+                .Union(newObjTypes)
+                .Union(typeMetaAttributeTypes)
+                .Union(fieldMetaAttributeTypes)
+                .Union(methodMetaAttributeTypes)
+                .Union(methodParameterMetaAttributeTypes)
+                .Union(ctorParameterTypes)
+                .Union(methodParameterTypes)
+                .Union(fieldTypes)
+                .Union(propertyTypes)
+                .Union(methodReturnTypes)
+                .Distinct();
+        }
     }
 }
---
+++ src/FluentCodeMetrics.Core/Cecil/AssemblyCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+using System.IO;
+
+namespace FluentCodeMetrics.Core.Cecil
+{
+    public static class AssemblyCache
+    {
+        private readonly static Dictionary<string, AssemblyDefinition>
+            Dictionary = new Dictionary<string, AssemblyDefinition>();
+
+        public static AssemblyDefinition Load(string assemblyName)
+        {
+            if (!File.Exists(assemblyName))
+                assemblyName = assemblyName + ".dll";
+
+            if (!Dictionary.ContainsKey(assemblyName))
+                Dictionary.Add(
+                    assemblyName,
+                    AssemblyDefinition.ReadAssembly(assemblyName)
+                    );
+
+            return Dictionary[assemblyName];
+        }
+    }
+}
